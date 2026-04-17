09f9c0f0968d5f198f9b2fe5c3857593edc66dc6
Luan Oliveira de Souza <luanvisouza037@gmail.com>
2026-04-16 22:39:12 -0300

    Comecando a criar a Api de Controle de Dieta

---
+++ ControleDietaApi/Models/User.cs
@@ -0,0 +1,14 @@
+namespace ControleDietaApi.Models;
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string? MyProperty { get; set; }
+    public int Idade { get; set; }
+    public decimal Peso { get; set; }
+    public decimal Altura { get; set; }
+    public string Meta { get; set; } = "Emagrecer";
+
+    public string? MetaDiaria { get; set; }
+}
